fefeadaaa202fb946384ee6748266f287818c370
marcelomoresco <marcelo.moresco@catolicasc.edu.br>
2024-01-27 11:29:54 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Marcelo Moresco on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
